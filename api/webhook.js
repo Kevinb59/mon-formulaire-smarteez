e8847a9bbd887de5351051adb24ebb2575bf1c13
Kevinb59 <130929760+Kevinb59@users.noreply.github.com>
2024-10-19 23:45:57 +0200

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,42 +1,16 @@
-const sgMail = require('@sendgrid/mail');
-
-// Configuration de la clé API de SendGrid
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const fetch = require('node-fetch');
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    console.log('---- Requête reçue ----');
-    console.log('Headers:', req.headers);
-    console.log('Corps de la requête:', req.body);
-
     // Retourner immédiatement 200 à Stripe pour confirmer la réception
     res.status(200).send('Webhook reçu');
 
-    // Préparation du corps de l'email
-    const msg = {
-      to: 'sasmarteez@gmail.com',  // Adresse de réception
-      from: 'sasmarteez@gmail.com',  // Adresse vérifiée sur SendGrid
-      subject: 'Test SendGrid avec logs détaillés',
-      text: 'Ceci est un email de test envoyé avec SendGrid. Voici le contenu de la requête Stripe: ' + JSON.stringify(req.body),
-      html: `<strong>Ceci est un test</strong><br/><br/>Contenu de la requête Stripe: <pre>${JSON.stringify(req.body)}</pre>`
-    };
-
-    // Tentative d'envoi de l'email via SendGrid
-    try {
-      console.log('Tentative d\'envoi de l\'email...');
-      const response = await sgMail.send(msg);
-      console.log('Réponse de SendGrid:', response);
-    } catch (error) {
-      console.error('Erreur lors de l\'envoi de l\'email avec SendGrid:', error);
-
-      // Log des détails d'erreur retournés par SendGrid
-      if (error.response) {
-        console.error('Réponse d\'erreur de SendGrid:', error.response.body);
-      }
-    }
+    // Exécuter le script Google Apps sans envoyer de données
+    await fetch('https://script.google.com/macros/s/AKfycbwhcuqH8Gomtd56Jn5RkhM_9MqaVqamvHUd1yjgDvetXLTovHZpbPSWQNppNVbClVrfKg/exec', {
+      method: 'GET', // Appel GET pour exécuter le script sans transmettre de données
+    });
   } else {
     res.setHeader('Allow', 'POST');
-    console.log('Méthode non autorisée: Tentative avec une autre méthode que POST');
     res.status(405).end('Méthode non autorisée');
   }
 }
